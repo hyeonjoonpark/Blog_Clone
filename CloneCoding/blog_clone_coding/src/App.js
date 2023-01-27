97e9cc20b0145303ed62c3f790f1fee2503af762
hyeonjoonpark <pjjoon1379@gmail.com>
2023-01-27 21:18:28 +0900

    ADD : modal

--- CloneCoding/blog_clone_coding/src/App.js
+++ CloneCoding/blog_clone_coding/src/App.js
@@ -1,30 +1,32 @@
 /* eslint-disable */
-
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
 function Modal() {
-  <div className='modal'>
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-  </div>
+  return (
+    <div className='modalPage'>
+      <p>제목<input className='inputModal' placeholder='제목을 입력해주세요'></input></p><br></br>
+      <p>내용<textarea className='textareaModal' placeholder='제목을 입력해주세요'></textarea></p>
+      <button type='submit' className='submitBtn'>업로드</button>
+    </div>
+  )
 }
 
 function App() {
-  let [title, setTitle] = useState(['학교 이미지와 실체', '부산소마고에 대하여', '아침운동이 X 같은 이유']);
-  let [good, goodAdd] = useState(0);
+  let [title, setTitle] = useState(['학교의 미친 계획', '"집 가고 싶다" 라는 말', '아침운동이 X 같은 이유']);
+  let [like, setLike] = useState(0);
+  let [hate, setHate] = useState(0);
+  let [modal, setModal] = useState(false);
 
-  function like() {
-    goodAdd(good+1);
+  function likeFn() {
+    setLike(like+1);
     alert("좋아요를 눌렀습니다");
   }
 
-  function titleChange() {
-    let copyTitle = [...title];
-    copyTitle[1] = 'ㅂㅅ소마고에 대하여';
-    setTitle(copyTitle);
+  function hateFn() {
+    setHate(hate+1);
+    alert("싫어요를 눌렀습니다");
   }
 
   function sortTitle() {
@@ -36,30 +38,34 @@ function App() {
   return (
     <div className="App">
       <div className="navBar">
-        <h4>Blog</h4>
-      </div>
-      <div>
-        <button className="btn" onClick={ titleChange }>change</button>
-        <button className="btn" onClick={ sortTitle }>정렬하기</button>
-      </div>
+        <h4>BSM Blog</h4>
+        <div>
+          <button className='btn'>BSM으로 시작하기</button>
+          <button className='btn' onClick={ ()=>{ setModal(!modal)} }>ㅂㅅ소마고에 업로드 하기</button>
+          <button className="btn" onClick={ sortTitle }>정렬하기</button>
+        </div>
 
+      </div>
       <div className="list">
-        <h4>{ title[0] } <button className="likeBtn" onClick={ like }>👍🏻</button> {good} </h4>
+        <h4>{ title[0] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
+        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate}  
+        </h4>
         <p>1월 23일 발행</p>
       </div>
   
       <div className="list">
-        <h4>{ title[1] } <button className="likeBtn" onClick={ like }>👍🏻</button> {good}</h4>
+        <h4>{ title[1] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
+        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate} </h4>
         <p>1월 23일 발행</p>
       </div>
 
       <div className="list">
-        <h4>{ title[2] } <button className="likeBtn" onClick={ like }>👍🏻</button> {good}</h4>
+        <h4>{ title[2] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
+        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate} </h4>
         <p>1월 23일 발행</p>
       </div>
 
-      <Modal></Modal>
-
+      {modal === true ? <Modal /> : null}
     </div>
   );
 }
