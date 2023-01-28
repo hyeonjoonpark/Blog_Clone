58c49490a857782d01a67aaa5bc2fdb2741122ca
hyeonjoonpark <pjjoon1379@gmail.com>
2023-01-28 12:46:48 +0900

    ADD : map

--- CloneCoding/blog_clone_coding/src/App.js
+++ CloneCoding/blog_clone_coding/src/App.js
@@ -6,28 +6,29 @@ import React, {useState} from 'react';
 function Modal() {
   return (
     <div className='modalPage'>
-      <p>제목<input className='inputModal' placeholder='제목을 입력해주세요'></input></p><br></br>
-      <p>내용<textarea className='textareaModal' placeholder='제목을 입력해주세요'></textarea></p>
+      <p>제목<input className='inputModal' placeholder='제목을 입력해주세요' method='post'></input></p>
+      <p>내용<textarea className='textareaModal' placeholder='제목을 입력해주세요' method='post'></textarea></p>
       <button type='submit' className='submitBtn'>업로드</button>
+    </div>
+  )
+}
+
+function CommentModal(){
+  return (
+    <div className='comment'>
+      댓글 <input className="commentInput" method='post'></input> 
     </div>
   )
 }
 
 function App() {
   let [title, setTitle] = useState(['학교의 미친 계획', '"집 가고 싶다" 라는 말', '아침운동이 X 같은 이유']);
-  let [like, setLike] = useState(0);
-  let [hate, setHate] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [hate, setHate] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-
-  function likeFn() {
-    setLike(like+1);
-    alert("좋아요를 눌렀습니다");
-  }
-
-  function hateFn() {
-    setHate(hate+1);
-    alert("싫어요를 눌렀습니다");
-  }
+  let [commentModal, setCommentModal] = useState(false);
+  let month = 3;
+  let day = 27;
 
   function sortTitle() {
     let copyTitle = [...title];
@@ -35,39 +36,41 @@ function App() {
     setTitle(copyTitle);
   }
 
+
   return (
     <div className="App">
       <div className="navBar">
         <h4>BSM Blog</h4>
         <div>
           <button className='btn'>BSM으로 시작하기</button>
-          <button className='btn' onClick={ ()=>{ setModal(!modal)} }>ㅂㅅ소마고에 업로드 하기</button>
+          <button className='btn' onClick={ ()=>{setModal(!modal)} }>게시물 작성하기</button>
           <button className="btn" onClick={ sortTitle }>정렬하기</button>
         </div>
-
       </div>
-      <div className="list">
-        <h4>{ title[0] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
-        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate}  
-        </h4>
-        <p>1월 23일 발행</p>
-      </div>
-  
-      <div className="list">
-        <h4>{ title[1] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
-        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate} </h4>
-        <p>1월 23일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{ title[2] } <button className="likeBtn" onClick={ likeFn }>👍🏻</button> {like}
-        <button className="hateBtn" onClick={ hateFn }>👍🏻</button> {hate} </h4>
-        <p>1월 23일 발행</p>
-      </div>
-
-      {modal === true ? <Modal /> : null}
-    </div>
-  );
+      {title.map((title, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4>{ title }<button className="likeBtn" onClick={ () => {
+              let likeCnt = [...like];
+              likeCnt[i]++;
+              setLike(likeCnt);
+            } }>👍🏻</button> {like[i]}
+              <button className="hateBtn" onClick={ () => {
+                let hateCnt = [...hate];
+                hateCnt[i]++;
+                setHate(hateCnt);
+              } }>👍🏻</button> {hate[i]}  
+            </h4>
+            <p><a>{month}월 {day}일 발행</a><a onClick={ ()=>{setCommentModal(!commentModal)} }>댓글 작성하기</a></p>
+            {commentModal === true ? <CommentModal /> : null}
+            { modal === true ? <Modal /> : null }
+          </div>
+        )
+      })
+    }
+  </div>
+  )
 }
+
 
 export default App;
